82f11d22a68c959f3baf5b1a5e4a595a14ef1391
Raza Mahmood <razammahmood@gmail.com>
2023-10-21 03:52:48 -0400

    remove unused

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,6 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
       cursor: {
         'w95-auto': 'url(/cursors/auto.cur), auto',
         'w95-pointer': 'url(/cursors/pointer.cur), pointer',
