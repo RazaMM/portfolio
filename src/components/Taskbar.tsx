6d2dbe54c9b835746956cd85edfc33cb60c43e8d
Raza Mahmood <razammahmood@gmail.com>
2023-10-21 04:31:44 -0400

    add start menu button

--- src/components/Taskbar.tsx
+++ src/components/Taskbar.tsx
@@ -2,12 +2,23 @@
 
 import React from "react";
 import { useTime } from "@/hooks/useTime";
+import Image from "next/image";
+import logo from '@/img/logo.png';
+
+function StartButton({ onClick }: { onClick: () => void }) {
+  return (
+    <button className='flex gap-1 px-1 h-full items-center justify-center shadow-w95 cursor-w95-pointer active:shadow-w95-inverted' onClick={onClick}>
+      <Image src={logo} alt='Portfolio logo' className='h-full' />
+      <span className='mr-1'>Start</span>
+    </button>
+  )
+}
 
 function Clock() {
   const time = useTime('minute');
 
   return (
-    <div className='my-2 ml-auto mr-2 px-3 shadow-w95-inverted-thin'>
+    <div className='ml-auto px-3 shadow-w95-inverted-thin'>
       {time.toLocaleTimeString(undefined, {
         hour: 'numeric',
         minute: '2-digit'
@@ -19,8 +30,8 @@ function Clock() {
 export default function Taskbar() {
 
   return (
-    <div className='flex items-center w-screen bg-w95-grey shadow-w95 h-10'>
-
+    <div className='flex items-center w-screen bg-w95-grey shadow-w95 h-10 p-2'>
+      <StartButton onClick={() => {}}/>
 
       <Clock/>
     </div>
