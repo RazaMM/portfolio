589d18d93b33e263ba8f2ad4915ac87d9aa4c76c
Raza Mahmood <razammahmood@gmail.com>
2023-12-16 19:42:44 -0500

    update logic in Biography.tsx

--- src/components/programs/Biography.tsx
+++ src/components/programs/Biography.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React from 'react';
 import icon from '@/img/bio.png';
 import { type Program } from '@/components/programs';
@@ -5,6 +7,27 @@ import Image from 'next/image';
 import me from '@/img/me.png';
 
 const Biography: React.FC = () => {
+  const date = new Date();
+
+  const today = {
+    year: date.getFullYear(),
+    month: date.getMonth() + 1,
+    date: date.getDate(),
+  };
+
+  const birthday = {
+    year: 1999,
+    month: 2,
+    date: 13,
+  };
+
+  let age = today.year - birthday.year;
+  const isBirthday = today.month === birthday.month && today.date === birthday.date;
+
+  if (today.month < birthday.month || (today.month === birthday.month && today.date < birthday.date)) {
+    age--;
+  }
+
   return (
     <div className='max-w-screen flex h-[30rem] max-h-screen min-h-full w-72 min-w-full flex-col items-center justify-center gap-4 bg-white p-2 text-center'>
       <Image
@@ -12,16 +35,22 @@ const Biography: React.FC = () => {
         alt={'Raza Mahmood wearing a black shirt and sunglasses'}
         className='w-56 select-none rounded-sm'
       />
+
       <h1 className='text-xl'>Raza Mahmood</h1>
-      <p>I am a 24 year old software developer who specializes in front end web development.</p>
+
+      <p>
+        I am a {age} year old {isBirthday && '(happy birthday to me!)'} software developer who specializes in front end
+        web development.
+      </p>
+
       <div className='flex flex-col'>
-        <a className='text-w95-blue' href='mailto:razammahmood@gmail.com'>
+        <a className='text-w95-blue hover:underline' href='mailto:razammahmood@gmail.com'>
           razammahmood@gmail.com
         </a>
-        <a className='text-w95-blue' href='https://github.com/RazaMM'>
+        <a className='text-w95-blue hover:underline' href='https://github.com/RazaMM'>
           github.com/RazaMM
         </a>
-        <a className='text-w95-blue' href='https://www.linkedin.com/in/rmmahmood/'>
+        <a className='text-w95-blue hover:underline' href='https://www.linkedin.com/in/rmmahmood/'>
           linkedin.com/in/rmmahmood/
         </a>
       </div>
