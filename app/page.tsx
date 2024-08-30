3296b8b5575d9b424628a9a88ea4775468fe805b
Raza Mahmood <razammahmood@gmail.com>
2024-08-29 22:00:51 -0400

    working on program context

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,47 @@
 'use client';
 
-import programs from '@/components/programs';
-import React from 'react';
+import programs, { Program } from '@/components/programs';
+import React, { useState } from 'react';
 import Desktop from '@/components/desktop';
 import Window from '@/components/window';
+import ProgramContext from '@/utils/program-context';
 
 export default function Home() {
   const test = programs[0];
+  const [open, setOpen] = useState<Program[]>([]);
+  const [active, setActive] = useState<Program | null>(null);
+  const [stackingOrder, setStackingOrder] = useState<Program[]>([]);
 
   return (
-    <>
+    <ProgramContext.Provider
+      value={{
+        open: (program: Program) => {
+          if (!open.some((p) => p.id === program.id)) {
+            setOpen([...open, program]);
+          }
+
+          setStackingOrder([program, ...stackingOrder.filter((p) => p.id !== program.id)]);
+          setActive(program);
+        },
+        close: (program: Program) => {
+          if (active?.id === program.id) {
+            setActive(null);
+          }
+
+          setOpen(open.filter((p) => p.id !== program.id));
+          setStackingOrder(stackingOrder.filter((p) => p.id !== program.id));
+        },
+        getOpen: () => [...open],
+        getActive: () => active,
+        setActive: (program: Program) => {
+          setActive(program);
+          setStackingOrder([program, ...stackingOrder.filter((p) => p.id !== program.id)]);
+        },
+        getStackingOrder: () => [...stackingOrder],
+      }}
+    >
       <Desktop />
       <Window program={test} active={true} />
-    </>
+    </ProgramContext.Provider>
   );
 }
--- utils/program-context.ts
+++ utils/program-context.ts
@@ -1,3 +1,22 @@
 import { createContext } from 'react';
+import { type Program } from '@/components/programs';
 
-export const ProgramContext = createContext({});
+export type ProgramContextValue = {
+  open: (program: Program) => void;
+  close: (program: Program) => void;
+  getOpen: () => Program[];
+  getActive: () => Program | null;
+  setActive: (program: Program) => void;
+  getStackingOrder: () => Program[];
+};
+
+export const ProgramContext = createContext<ProgramContextValue>({
+  open: (_program: Program) => {},
+  close: (_program: Program) => {},
+  getOpen: () => [],
+  getActive: () => null,
+  setActive: (_program: Program) => {},
+  getStackingOrder: () => [],
+});
+
+export default ProgramContext;
