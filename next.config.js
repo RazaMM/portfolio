68c287dcb888461e4bb98a5b124d5dd46ee6cbf6
Raza Mahmood <razammahmood@gmail.com>
2024-09-22 20:54:00 -0400

    Add webpack-graphql-loader to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.module.rules.push({
+      test: /\.gql|.graphql$/,
+      use: {
+        loader: 'webpack-graphql-loader',
+        options: {
+          removeUnusedFragments: true,
+          output: 'document',
+          validate: false,
+          minify: true,
+        },
+      },
+    });
+    return config;
+  },
+};
 
 module.exports = nextConfig;
