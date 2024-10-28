d79b12785f9f35679c3ff501f4ff09bd5b394f9b
Raza Mahmood <razammahmood@gmail.com>
2024-10-28 01:58:51 -0400

    add spacing for desktop icons

--- components/desktop.tsx
+++ components/desktop.tsx
@@ -26,7 +26,7 @@ export const Desktop = () => {
   const context = useContext(ProgramContext);
 
   return (
-    <div className='relative flex h-full w-fit select-none flex-col flex-wrap items-center gap-4'>
+    <div className='relative flex h-full w-fit select-none flex-col flex-wrap items-center gap-4 px-2'>
       {programs
         .filter((program) => program.includeInDesktop)
         .map((program, i) => (
