f65841b08739991f8fd8cf9ebaa14afe7d47fc63
Raza Mahmood <razammahmood@gmail.com>
2025-03-28 01:28:19 -0400

    fix: update import path for Desktop component in page.tsx

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Desktop from '@/components/desktop';
+import Desktop from '@/components/desktop/desktop';
 import Taskbar from '@/components/taskbar/taskbar';
 
 export default function BlogHome() {
