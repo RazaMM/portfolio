15542e9852d8893122e688b0ad05c44ae10962cd
Raza Mahmood <razammahmood@gmail.com>
2025-03-28 01:21:53 -0400

    fix: update Desktop component height calculation for improved layout

--- components/desktop/desktop.tsx
+++ components/desktop/desktop.tsx
@@ -6,7 +6,9 @@ export type DesktopProps = PropsWithChildren;
 
 export const Desktop = ({ children }: DesktopProps) => {
   return (
-    <div className='relative flex h-full w-fit flex-col flex-wrap items-center gap-4 p-2 select-none'>{children}</div>
+    <div className='relative flex h-[calc(100vh-theme(height.10))] w-fit flex-col flex-wrap items-center gap-4 p-2 select-none'>
+      {children}
+    </div>
   );
 };
 
