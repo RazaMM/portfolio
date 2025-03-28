c42ef9853324fd6478a0c43bffaee7bc21e75930
Raza Mahmood <razammahmood@gmail.com>
2025-03-28 15:39:34 -0400

    fix: adjust layout width for Window component and update prose styling

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -37,13 +37,13 @@ export default async function BlogLayout({ params }: { params: Promise<{ slug: s
       </Desktop>
 
       <Window name={title} active icon={{ src: Notepad, alt: '' }}>
-        <div className='w-dvw max-w-3xl bg-white p-2'>
+        <div className='w-dvw max-w-2xl bg-white p-2'>
           <div className='flex flex-col gap-2'>
             <h1 className='text-3xl'>{title}</h1>
             <span>Posted on {formatter.format(date)}</span>
           </div>
           <hr className='my-2' />
-          <div className='mx-auto prose'>
+          <div className='mx-auto prose max-w-none'>
             <Post />
           </div>
         </div>
