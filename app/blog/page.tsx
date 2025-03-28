57dac7082fcc970039df410393cef9a851a1812c
Raza Mahmood <razammahmood@gmail.com>
2025-03-28 15:53:29 -0400

    working on blog home page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -7,8 +7,28 @@ import HomePage from '@/img/homepage.png';
 import { TaskbarClock } from '@/components/taskbar/taskbar-clock';
 import Link from 'next/link';
 import { DesktopShortcut } from '@/components/desktop/desktop-shortcut';
+import Window from '@/components/window';
+import Notepad from '@/img/notepad.png';
+import { TaskbarContent } from '@/components/taskbar/taskbar-content';
+import { TaskbarButton } from '@/components/taskbar/taskbar-button';
+
+type BlogPost = {
+  title: string;
+  date: Date;
+  path: string;
+};
+
+const formatter = new Intl.DateTimeFormat();
 
 export default function BlogHome() {
+  const posts: BlogPost[] = [
+    {
+      title: 'Implementing Cubic Bezier Easing in JavaScript',
+      date: new Date('2022-02-24'),
+      path: '/blog/2025-03-28-implementing-cubic-bezier-easing-in-javascript',
+    },
+  ];
+
   return (
     <>
       <Desktop>
@@ -16,12 +36,37 @@ export default function BlogHome() {
           Back to Homepage
         </DesktopShortcut>
       </Desktop>
+
+      <Window name='My Blog Posts' active icon={{ src: Notepad, alt: '' }}>
+        <div className='w-dvw max-w-xl bg-white p-2'>
+          <div className='flex flex-col gap-2'>
+            {posts.map((post) => (
+              <Link
+                key={post.path}
+                href={post.path}
+                className='flex flex-col gap-2 border-b-2 border-w95-dark-grey p-2 hover:bg-w95-grey focus:bg-w95-grey'
+              >
+                <span className='text-xl'>{post.title}</span>
+                <span className='text-sm'>{formatter.format(post.date)}</span>
+              </Link>
+            ))}
+          </div>
+        </div>
+      </Window>
+
       <Taskbar>
         <TaskbarMenu title="Raza's Blog">
           <TaskbarMenuItem as={Link} href='/' icon={{ src: HomePage, alt: '' }}>
             Back to Homepage
           </TaskbarMenuItem>
         </TaskbarMenu>
+
+        <TaskbarContent>
+          <TaskbarButton onClick={undefined} active={true} icon={{ src: Notepad, alt: '' }}>
+            My Blog Posts
+          </TaskbarButton>
+        </TaskbarContent>
+
         <TaskbarClock />
       </Taskbar>
     </>
