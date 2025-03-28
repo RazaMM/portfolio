c66198a1fd8621325942b58260bd279484a2bc8b
Raza Mahmood <razammahmood@gmail.com>
2025-03-27 23:42:01 -0400

    fix: remove unnecessary max-h-screen class from biography component

--- components/programs/biography.tsx
+++ components/programs/biography.tsx
@@ -29,7 +29,7 @@ const Biography: React.FC = () => {
   }
 
   return (
-    <div className='flex h-[30rem] max-h-screen min-h-full w-72 max-w-screen min-w-full flex-col items-center justify-center gap-4 bg-white p-2 text-center'>
+    <div className='flex h-[30rem] min-h-full w-72 max-w-screen min-w-full flex-col items-center justify-center gap-4 bg-white p-2 text-center'>
       <Image
         src={me}
         alt={'Raza Mahmood wearing a black shirt and sunglasses'}
