fc0c2796b6236a23008d82c5dac1ee6c5e03d66b
Raza Mahmood <razammahmood@gmail.com>
2025-03-29 03:05:01 -0400

    feat: refactor ShutdownScreen component to use tailwind-variants for improved visibility handling

--- components/taskbar/shutdown-screen.tsx
+++ components/taskbar/shutdown-screen.tsx
@@ -1,16 +1,30 @@
 import React, { MouseEventHandler } from 'react';
 import { twJoin } from 'tailwind-merge';
+import { tv } from 'tailwind-variants';
 
 export const ShutdownScreen = ({ visible, onClick }: { visible: boolean; onClick: MouseEventHandler }) => {
+  const classes = tv({
+    slots: {
+      base: 'fixed top-0 left-0 z-50 flex h-screen w-screen items-center justify-center bg-black text-6xl',
+      text: 'max-w-xl text-center text-orange-400',
+    },
+    variants: {
+      visible: {
+        true: {
+          base: 'visible',
+          text: 'visible',
+        },
+        false: {
+          base: 'invisible',
+          text: 'invisible',
+        },
+      },
+    },
+  })({ visible });
+
   return (
-    <div
-      onClick={onClick}
-      className={twJoin(
-        'fixed top-0 left-0 z-50 flex h-screen w-screen items-center justify-center bg-black text-6xl',
-        !visible && 'hidden'
-      )}
-    >
-      <span className='max-w-xl text-center text-orange-400'>It&apos;s now safe to turn off your computer.</span>
+    <div onClick={onClick} className={classes.base()}>
+      <span className={classes.text()}>It&apos;s now safe to turn off your computer.</span>
     </div>
   );
 };
