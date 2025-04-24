e4d25d01f6747b94fb94c3ab154d30f79d248a33
Raza Mahmood <razammahmood@gmail.com>
2025-04-24 15:21:22 -0400

    Add playground page with desktop and taskbar components

---
+++ app/playground/page.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import Desktop from '@/components/desktop/desktop';
+import Taskbar from '@/components/taskbar/taskbar';
+import { TaskbarMenu } from '@/components/taskbar/taskbar-menu';
+import { TaskbarMenuItem } from '@/components/taskbar/taskbar-menu-item';
+import HomePage from '@/img/homepage.png';
+import { TaskbarClock } from '@/components/taskbar/taskbar-clock';
+import Link from 'next/link';
+import { DesktopShortcut } from '@/components/desktop/desktop-shortcut';
+import { TaskbarContent } from '@/components/taskbar/taskbar-content';
+//import { TaskbarButton } from '@/components/taskbar/taskbar-button';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: "Playground | Raza Mahmood's Portfolio",
+  description: 'Random games and other playground stuff.',
+};
+
+export default async function FunHome() {
+  return (
+    <>
+      <Desktop>
+        <DesktopShortcut as={Link} href='/' icon={{ src: HomePage, alt: '' }}>
+          Back to Homepage
+        </DesktopShortcut>
+      </Desktop>
+
+      <Taskbar>
+        <TaskbarMenu title="Raza's Playground">
+          <TaskbarMenuItem as={Link} href='/' icon={{ src: HomePage, alt: '' }}>
+            Back to Homepage
+          </TaskbarMenuItem>
+        </TaskbarMenu>
+
+        <TaskbarContent>
+          {/*<TaskbarButton active={true} icon={{ src: Notepad, alt: '' }}>
+            My Blog Posts
+          </TaskbarButton>*/}
+        </TaskbarContent>
+
+        <TaskbarClock />
+      </Taskbar>
+    </>
+  );
+}
